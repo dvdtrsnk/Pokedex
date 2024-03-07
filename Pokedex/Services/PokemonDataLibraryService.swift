4f4b4164f39e06405fe0ef20442bfcfafe675943
David Tresnak <s@tresnak.cc>
2024-03-07 10:07:45 +0100

    Pokemon Library Import

---
+++ Pokedex/Services/PokemonDataLibraryService.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDataLibraryService.swift
+//  Pokedex
+//
+//  Created by David Třešňák on 07.03.2024.
+//
+
+import Foundation
+import PokemonAPI
+
+protocol PokemonDataProtocol {
+    
+}
+
+struct PokemonDataLibraryService: PokemonDataProtocol {
+
+    
+}
