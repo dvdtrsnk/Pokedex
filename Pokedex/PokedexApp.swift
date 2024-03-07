423586e5bd225685f08469eaa00f38120ff71cbb
David Tresnak <s@tresnak.cc>
2024-03-07 09:45:43 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by David Třešňák on 07.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
